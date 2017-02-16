ad7f84ecdd0a4a9bd0aaaa71f3c0a50aafdb8fb4
jweboy <1259524459@qq.com>
2017-02-16 21:55:38 +0800

    init sell-webapp

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  goods
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  selleer
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="css">
+</style>
