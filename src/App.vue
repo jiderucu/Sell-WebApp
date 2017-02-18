a6680bb147ca95f5837e1bd8603dd14e5aa71ed4
jweboy <1259524459@qq.com>
2017-02-18 22:34:14 +0800

    add stylus, add header content

--- src/App.vue
+++ src/App.vue
@@ -20,50 +20,68 @@
   // import {urlParse} from 'common/js/util';
   import header from 'components/header/header';
 
-  // const ERR_OK = 0;
+  const ERR_OK = 0;
 
   export default {
-    data() {
-      return {
+    // data() {
+      // return {
         // seller: {
         //   id: (() => {
         //     let queryParam = urlParse();
         //     return queryParam.id;
         //   })()
         // }
-      };
+    //   };
+    // },
+    data() {
+      return {
+        seller: {
+
+        }
+      }
     },
     created() {
+      this.$http.get('/api/seller').then((response) => {
+        console.dir(response);
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
+    // created() {
       // this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
       //   response = response.body;
       //   if (response.errno === ERR_OK) {
       //     this.seller = Object.assign({}, this.seller, response.data);
       //   }
       // });
-    },
+    // },
     components: {
       'v-header': header
     }
   };
 
 </script>
-<style lang="css">
-  #app .tab{
-    display: flex;
-    width: 100%;
-    height: 40px;
-    line-height: 40px;
-  }
-  #app .tab .tab-item{
-    flex: 1;
-    text-align: center;
-  }
-  #app .tab .tab-item a{
-    display: block;
-    font-size: 14px;
-    color: rgb(77, 85, 93);
-  }
-  #app .tab .tab-item a.active{
-    color: rgb(240,20,20);
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixin.styl'
+
+  #app
+    .tab
+      display: flex
+      width:100%
+      height:40px
+      line-height:40px
+      // border-bottom:1px solid rgba(7, 17, 27, 0.1)
+      border-1px(rgba(7, 17, 27,1))
+      .tab-item
+          flex:1
+          text-align:center
+          & > a
+            display:block
+            font-size:14px
+            color:rgb(77, 85, 93)
+            &.active
+              color: rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* jshint esversion:6 */
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
@@ -6,7 +7,7 @@ import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
 
-// import 'common/stylus/index.styl';
+import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
